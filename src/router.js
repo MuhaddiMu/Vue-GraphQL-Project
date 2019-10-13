36c1e5f01d7ce4b6110a4f0a5394e20d8cfe27d3
MuhaddiMu <Muhaddisshah@gmail.com>
2019-10-13 23:58:37 +0500

    :tada: Inital Commit, Add Graph QL & Apollo

--- src/router.js
+++ src/router.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
+import ApolloExample from './components/ApolloExample'
 
 Vue.use(Router)
 
@@ -12,6 +13,11 @@ export default new Router({
       path: '/',
       name: 'home',
       component: Home
+    },
+    {
+      path: '/Apollo',
+      name: 'Apollo',
+      component: ApolloExample
     },
     {
       path: '/about',
